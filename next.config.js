abc9424a7362d2e2a53d614f3c3855d510c9f44c
Rubén Alonso <ruben.alonsocorral@gmail.com>
2024-03-20 13:48:41 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -35,14 +35,6 @@ const nextConfig = {
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
         ],
       },
       {
