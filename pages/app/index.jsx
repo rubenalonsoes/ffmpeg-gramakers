e5da1b638bcf05adb02788b5d89df9b8864d6467
Rubén Alonso <ruben.alonsocorral@gmail.com>
2024-03-20 13:35:02 +0100

    Update index.jsx
    CORS vercel

--- pages/app/index.jsx
+++ pages/app/index.jsx
@@ -10,6 +10,31 @@ import JSZip from "jszip";
 
 const { Dragger } = Upload;
 
+const enableCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
+  // specific logic for the preflight request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const currentDate = new Date()
+  res.end(currentDate.toString())
+}
+
+module.exports = enableCors(handler)
+
 const App = () => {
   const [spinning, setSpinning] = useState(false);
   const [tip, setTip] = useState(false);
