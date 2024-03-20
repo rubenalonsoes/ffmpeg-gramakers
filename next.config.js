4809547082435e09bd14b910def644e6ee01da60
Rubén Alonso <ruben.alonsocorral@gmail.com>
2024-03-20 12:56:10 +0100

    Update next.config.js
    Cabeceras de CORS añadidas

--- next.config.js
+++ next.config.js
@@ -31,6 +31,10 @@ const nextConfig = {
       {
         source: "/",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://ffmpeg-gramakers.vercel.app" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           {
             key: "Cross-Origin-Embedder-Policy",
             value: "require-corp",
