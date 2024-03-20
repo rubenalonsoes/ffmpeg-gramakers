1fdd1990e2612ac4b9bb57e52c0ca67428bbb943
Rubén Alonso <ruben.alonsocorral@gmail.com>
2024-03-20 14:14:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "(.*)",
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "* || https://ffmpeg-gramakers.vercel.app" }, // replace this your actual origin
@@ -41,7 +41,7 @@ const nextConfig = {
           },
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
+            value: "*",
           },
         ],
       },
